38727240561d637f6f4fee6cc94cf1447acbfbc3
Sujan Kandeepan <kandeeps@mcmaster.ca>
2020-10-28 00:07:11 -0400

    Added basic events page for prototype

--- events.js
+++ events.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+import { Card } from 'react-native-elements';
 
 /**
  * This is the default construct for a class.
@@ -12,8 +13,16 @@ export default class Events extends React.Component {
     }
 
     render() {
+        // let width = Dimensions.get('window').width;
+        // let height = width * 677 / 974;
         return (
-            <Text>Events page</Text>
+            <Card>
+                <Image source={require("./assets/2019-Reunion-Flyer.png")}
+                    style={{
+                        width: "100%", height: "auto",
+                        aspectRatio: 974 / 677
+                    }} />
+            </Card>
         );
     }
 }
