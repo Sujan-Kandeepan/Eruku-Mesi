bc9305f80ecf5deae9597624973bd78641ce2617
Sujan Kandeepan <kandeeps@mcmaster.ca>
2020-12-31 13:43:33 -0500

    Pass route name as prop instead of duplicating props

--- App.js
+++ App.js
@@ -13,16 +13,16 @@ const admin = true;
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
-// Display basic text in app app pages not yet implemented
-const SimpleText = ({ text }) => <View style={styles.container}><Text>{text}</Text></View>;
+// Display basic text for route name in app pages not yet implemented
+const SimpleText = ({ route }) => <View style={styles.container}><Text>{route.name}</Text></View>;
 
 // Common layout/logic for all app pages
-const AppPage = ({ children, navigation, title }) => (
+const AppPage = ({ children, navigation, route }) => (
   // Workaround for displaying header within drawer nav (framework limitation):
   // https://github.com/react-navigation/react-navigation/issues/1632#issuecomment-305291994
   <NavigationContainer style={styles.container} independent>
     <Stack.Navigator>
-      <Stack.Screen name={title} options={{
+      <Stack.Screen name={route.name} options={{
         headerLeft: () =>
           // Display hamburger icon with workaround for padding bug in library
           <View>
@@ -43,55 +43,55 @@ const AppPage = ({ children, navigation, title }) => (
 
 // Quickly define pages/forms, refactor into separate modules later
 const NewsAndEventsPage = (props) =>
-  <AppPage {...props} title='News and Events'>
-    <SimpleText text='News and Events' />
+  <AppPage {...props}>
+    <SimpleText {...props} />
   </AppPage>;
 const EventsCalendar = (props) =>
-  <AppPage {...props} title='Events Calendar'>
-    <SimpleText text='Events Calendar' />
+  <AppPage {...props}>
+    <SimpleText {...props} />
   </AppPage>;
 const MessagesPage = (props) =>
-  <AppPage {...props} title='Messages'>
-    <SimpleText text='Messages' />
+  <AppPage {...props}>
+    <SimpleText {...props} />
   </AppPage>;
 const MediaContentPage = (props) =>
-  <AppPage {...props} title='Media Content'>
-    <SimpleText text='Media Content' />
+  <AppPage {...props}>
+    <SimpleText {...props} />
   </AppPage>;
 const EventForm = (props) =>
-  <AppPage {...props} title='Manage Events'>
-    <SimpleText text='Manage Events' />
+  <AppPage {...props}>
+    <SimpleText {...props} />
   </AppPage>;
 const NewsStoryForm = (props) =>
-  <AppPage {...props} title='Manage News Stories'>
-    <SimpleText text='Manage News Stories' />
+  <AppPage {...props}>
+    <SimpleText {...props} />
   </AppPage>;
 const MediaContentForm = (props) =>
-  <AppPage {...props} title='Manage Media Content'>
-    <SimpleText text='Manage Media Content' />
+  <AppPage {...props}>
+    <SimpleText {...props} />
   </AppPage>;
 const SettingsPage = (props) =>
-  <AppPage {...props} title='Settings'>
-    <SimpleText text='Settings' />
+  <AppPage {...props}>
+    <SimpleText {...props} />
   </AppPage>;
 const FeedbackForm = (props) =>
-  <AppPage {...props} title='Feedback'>
-    <SimpleText text='Feedback' />
+  <AppPage {...props}>
+    <SimpleText {...props} />
   </AppPage>;
 const EventNotificationFormat = (props) =>
-  <AppPage {...props} title='Event Notification'>
-    <SimpleText text='Event Notification' />
+  <AppPage {...props}>
+    <SimpleText {...props} />
   </AppPage>;
 const AuthenticationForm = (props) =>
-  <AppPage {...props} title='Authentication'>
-    <SimpleText text='Authentication' />
+  <AppPage {...props}>
+    <SimpleText {...props} />
   </AppPage>;
 
 export default function App() {
   return (
     // Reference: https://reactnavigation.org/docs/drawer-based-navigation/
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName='Home'>
+      <Drawer.Navigator initialRouteName='News and Events'>
         {/* Section: Content and Communication */}
         <Drawer.Screen name='News and Events' component={NewsAndEventsPage} />
         <Drawer.Screen name='Events Calendar' component={EventsCalendar} />
