dfff03a374b4b630c88c6c0adc027e0807adb443
Sujan Kandeepan <kandeeps@mcmaster.ca>
2021-01-03 10:22:11 -0500

    Updates to drawer

--- App.js
+++ App.js
@@ -3,17 +3,15 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { Avatar, Caption, DarkTheme as DarkDrawerTheme, DefaultTheme as LightDrawerTheme,
-  Drawer as CustomDrawer, Provider, Snackbar, Title, useTheme } from 'react-native-paper';
+  Drawer as CustomDrawer, Provider, Snackbar, Title } from 'react-native-paper';
 import { createDrawerNavigator, DrawerContentScrollView } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
 import { DarkTheme, DefaultTheme as LightTheme, NavigationContainer } from '@react-navigation/native';
 
 import EmptyPage from './components/EmptyPage';
 import SettingsPage from './components/SettingsPage';
 
-// Initialize drawer/stack navigators
+// Initialize drawer navigator
 const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
 
 // Custom light app/drawer theme, inherit defaults and modify few properties
 // Reference: https://callstack.github.io/react-native-paper/theming.html
@@ -26,7 +24,6 @@ const lightTheme = {
     primary: 'green',
     accent: 'darkgreen',
     background: LightTheme.colors.background,
-    drawer: LightTheme.colors.surface,
     statusBarText: 'dark',
     statusBarBackground: LightDrawerTheme.colors.background,
     text: 'black'
@@ -43,7 +40,6 @@ const darkTheme = {
     primary: 'green',
     accent: 'darkgreen',
     background: DarkTheme.colors.background,
-    drawer: DarkTheme.colors.background,
     statusBarText: 'light',
     statusBarBackground: DarkTheme.colors.background,
     text: 'lightgrey'
@@ -55,6 +51,7 @@ const NewsAndEventsPage = EmptyPage;
 const EventsCalendar = EmptyPage;
 const MessagesPage = EmptyPage;
 const MediaContentPage = EmptyPage;
+const InformationPage = EmptyPage;
 const FeedbackForm = EmptyPage;
 const NewsStoryForm = EmptyPage;
 const EventForm = EmptyPage;
@@ -62,11 +59,22 @@ const MediaContentForm = EmptyPage;
 const AuthenticationForm = EmptyPage;
 const EventNotificationFormat = EmptyPage;
 
+// Names of drawer items/pages
+const pages = {
+  newsAndEvents: 'News and Events',
+  eventsCalendar: 'Events Calendar',
+  messages: 'Messages',
+  mediaContent: 'Media Content',
+  information: 'Information',
+  settings: 'Settings',
+  feedback: 'Feedback'
+};
+
 // Repeated logic for custom drawer items
-const CustomDrawerItem = ({ action, icon, name, navigation, source, state }) =>
+const CustomDrawerItem = ({ action, icon, name, navigation, source, state, theme }) =>
   <CustomDrawer.Item label={name} active={name === state.routeNames[state.index]}
     // Icon names reference: https://oblador.github.io/react-native-vector-icons/
-    icon={() => <Icon name={icon} type={source} color='green' style={{ width: 25 }} />}
+    icon={() => <Icon name={icon} type={source} color={theme.colors.primary} style={{ width: 25 }} />}
     onPress={() => action ? action() : navigation.navigate(name)} />;
 
 // Custom drawer layout to override default drawer navigation screen list
@@ -74,8 +82,7 @@ const CustomDrawerItem = ({ action, icon, name, navigation, source, state }) =>
 // Reference: https://github.com/itzpradip/react-navigation-v5-mix/blob/master/screens/DrawerContent.js
 const CustomDrawerContent = (props) =>
   <DrawerContentScrollView {...props}
-    contentContainerStyle={{ backgroundColor: props.theme.colors.drawer,
-      flex: 1, justifyContent: 'space-between' }}>
+    contentContainerStyle={{ flex: 1, justifyContent: 'space-between' }}>
     <CustomDrawer.Section style={{ flexDirection: 'row', marginHorizontal: 15, marginVertical: 5 }}>
       <Avatar.Image source={{ // For local image: source={require('./assets/image.jpg')}
           uri: 'https://pbs.twimg.com/profile_images/3378852260/ea07b725a6331c3255c6283ae7ad97d0_400x400.jpeg'
@@ -89,21 +96,23 @@ const CustomDrawerContent = (props) =>
     <CustomDrawer.Section />
     {/* Force section to take up remaining height to push rest to bottom */}
     <CustomDrawer.Section style={{ flex: 1 }}>
-      <CustomDrawerItem {...props} name='News and Events'
+      <CustomDrawerItem {...props} name={pages.newsAndEvents}
         icon='newspaper-variant-outline' source='material-community' />
-      <CustomDrawerItem {...props} name='Events Calendar'
+      <CustomDrawerItem {...props} name={pages.eventsCalendar}
         icon='calendar-star' source='material-community' />
-      <CustomDrawerItem {...props} name='Messages'
+      <CustomDrawerItem {...props} name={pages.messages}
         icon='message-text-outline' source='material-community' />
-      <CustomDrawerItem {...props} name='Media Content'
+      <CustomDrawerItem {...props} name={pages.mediaContent}
         icon='photo-camera-back' source='material' />
+      <CustomDrawerItem {...props} name={pages.information}
+        icon='info-outline' source='material' />
     </CustomDrawer.Section>
     {/* Add divider above second section by creating empty section */}
     <CustomDrawer.Section />
     <CustomDrawer.Section style={{ marginBottom: 0 }}>
-      <CustomDrawerItem {...props} name='Settings'
+      <CustomDrawerItem {...props} name={pages.settings}
         icon='settings' source='material' />
-      <CustomDrawerItem {...props} name='Feedback'
+      <CustomDrawerItem {...props} name={pages.feedback}
         icon='feedback' source='material' />
     </CustomDrawer.Section>
   </DrawerContentScrollView>;
@@ -143,30 +152,31 @@ export default function App() {
     // Reference: https://reactnavigation.org/docs/drawer-based-navigation/
     <Provider theme={theme}>
       <NavigationContainer theme={theme}>
-        <Drawer.Navigator initialRouteName='News and Events'
+        <Drawer.Navigator initialRouteName={pages.newsAndEvents}
           drawerContentOptions={{ labelStyle: { color: theme.colors.text } }}
           drawerContent={props => <CustomDrawerContent {...props} theme={theme} />}>
-          <Drawer.Screen name='News and Events'
+          <Drawer.Screen name={pages.newsAndEvents}
             children={(props) => <NewsAndEventsPage {...props} theme={theme} />} />
-          <Drawer.Screen name='Events Calendar'
+          <Drawer.Screen name={pages.eventsCalendar}
             children={(props) => <EventsCalendar {...props} theme={theme} />} />
-          <Drawer.Screen name='Messages'
+          <Drawer.Screen name={pages.messages}
             children={(props) => <MessagesPage {...props} theme={theme} />} />
-          <Drawer.Screen name='Media Content'
+          <Drawer.Screen name={pages.mediaContent}
             children={(props) => <MediaContentPage {...props} theme={theme} />} />
-          <Drawer.Screen name='Settings' children={(props) =>
+          <Drawer.Screen name={pages.information}
+            children={(props) => <InformationPage {...props} theme={theme} />} />
+          <Drawer.Screen name={pages.settings} children={(props) =>
             <SettingsPage {...props} theme={theme} toggleTheme={toggleTheme} snackbar={snackbar}
               receiveNotifications={receiveNotifications} toggleNotifications={toggleNotifications} />} />
-          <Drawer.Screen name='Feedback'
+          <Drawer.Screen name={pages.feedback}
             children={(props) => <FeedbackForm {...props} theme={theme} />} />
         </Drawer.Navigator>
       </NavigationContainer>
       <StatusBar style={theme.colors.statusBarText}
         backgroundColor={theme.colors.statusBarBackground} />
-      <Snackbar visible={snackbarVisible} duration={3000}
+      <Snackbar visible={snackbarVisible} duration={3000} onDismiss={onDismissSnackbar}
         style={{ alignSelf: 'center', flexDirection: 'row', margin: 50, width: snackbarWidth || 'auto' }}
-        theme={{ colors: { onSurface: theme.colors.card, surface: theme.colors.text } }}
-        onDismiss={onDismissSnackbar}>
+        theme={{ colors: { onSurface: theme.colors.card, surface: theme.colors.text } }}>
         <Text adjustsFontSizeToFit={true}>{snackbarText}</Text>
       </Snackbar>
     </Provider>
