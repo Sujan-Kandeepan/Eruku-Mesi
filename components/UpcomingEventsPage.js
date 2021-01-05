457ab0e28cf1a2996c81ee64e9478156df7335b0
Sujan Kandeepan <kandeeps@mcmaster.ca>
2021-01-05 18:38:04 -0500

    Setup for events feed

--- components/UpcomingEventsPage.js
+++ components/UpcomingEventsPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
@@ -17,9 +19,14 @@ const Tab = createBottomTabNavigator();
 export default function UpcomingEventsPage(props) {
   const pages = {
     createEvent: 'Create Event',
+    viewEvent: id => `View Event ${id}`,
+    editEvent: id => `Edit Event ${id}`,
+    deleteEvent: id => `Delete Event ${id}`,
     listView: 'List View',
     calendarView: 'Calendar View'
   };
+  const events = [...Array(10).keys()].map(n =>
+    ({ id: n + 1, title: `Event ${n + 1}`, description: `Event ${n + 1} description` }));
   return (
     <AppPage {...props}>
       <NavigationContainer style={SharedStyles.container} theme={props.theme} independent>
@@ -31,16 +38,26 @@ export default function UpcomingEventsPage(props) {
                   onPress={() => localProps.navigation.push(pages.createEvent)} />}
               <NavigationContainer style={SharedStyles.container} theme={props.theme} independent>
                 {/* Reference: https://reactnavigation.org/docs/bottom-tab-navigator */}
-                <Tab.Navigator tabBarOptions={{
-                  // Reference: https://stackoverflow.com/a/63913237
-                  labelStyle: {
-                    fontSize: 14, position: 'absolute',
-                    top: 0, bottom: 0, left: 0, right: 0,
-                    textAlignVertical: 'center'
-                  },
-                }}>
-                  <Tab.Screen name={pages.listView} children={(localProps) =>
-                    <EmptyPage {...props} {...localProps} nested tab />} />
+                <Tab.Navigator tabBarOptions={{ labelStyle: {
+                  fontSize: 14, margin: 15, textAlignVertical: 'center'
+                }}}>
+                  <Tab.Screen name={pages.listView} children={() =>
+                    <FlatList data={events} renderItem={({ item }) =>
+                      <TouchableOpacity onPress={() => localProps.navigation.push(pages.viewEvent(item.id))}>
+                        <Card containerStyle={{
+                          borderColor: props.theme.colors.border,
+                          backgroundColor: props.theme.colors.card
+                        }}>
+                          <Text style={{ fontWeight: 'bold', color: props.theme.colors.text, marginBottom: 10 }}>
+                            {item.title}
+                          </Text>
+                          <Text style={{ color: props.theme.colors.text }}>
+                            {item.description}
+                          </Text>
+                        </Card>
+                      </TouchableOpacity>
+                    } keyExtractor={item => item.title}
+                      ListFooterComponent={<View style={{ height: 15 }} />} />} />
                   <Tab.Screen name={pages.calendarView} children={(localProps) =>
                     <EmptyPage {...props} {...localProps} nested tab />} />
                 </Tab.Navigator>
@@ -49,6 +66,25 @@ export default function UpcomingEventsPage(props) {
           {props.admin &&
             <Stack.Screen name={pages.createEvent} children={(localProps) =>
               <EventForm {...props} {...localProps} />} />}
+          {events.map(event =>
+            <Stack.Screen key={event.id} name={pages.viewEvent(event.id)} children={(localProps) =>
+              <AppPage {...props} {...localProps} nested>
+                {props.admin &&
+                  <Button {...props} {...localProps} text='Edit'
+                    onPress={() => localProps.navigation.push(pages.editEvent(event.id))} />}
+                {props.admin &&
+                  <Button {...props} {...localProps} text='Delete'
+                    onPress={() => localProps.navigation.push(pages.deleteEvent(event.id))} />}
+                <View style={SharedStyles.container}>
+                  <Text style={{ color: props.theme.colors.text }}>{localProps.route.name}</Text>
+                </View>
+              </AppPage>} />)}
+          {events.map(event =>
+            <Stack.Screen key={event.id} name={pages.editEvent(event.id)} children={(localProps) =>
+              <EventForm {...props} {...localProps} />} />)}
+          {events.map(event =>
+            <Stack.Screen key={event.id} name={pages.deleteEvent(event.id)} children={(localProps) =>
+              <EmptyPage {...props} {...localProps} nested cancel />} />)}
         </Stack.Navigator>
       </NavigationContainer>
     </AppPage>
