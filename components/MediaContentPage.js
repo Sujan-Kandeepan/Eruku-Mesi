6deb9800e28a9ca953e4daf751b33aa19bb6c794
Sujan Kandeepan <kandeeps@mcmaster.ca>
2021-01-05 10:55:53 -0500

    More setup for media content page

--- components/MediaContentPage.js
+++ components/MediaContentPage.js
@@ -9,13 +9,17 @@ import AppPage from './AppPage';
 import MediaContentForm from './MediaContentForm';
 import { Button } from '../shared/SharedComponents';
 import SharedStyles from '../shared/SharedStyles';
+import EmptyPage from './EmptyPage';
 
 // Initialize stack navigator
 const Stack = createStackNavigator();
 
 export default function MediaContentPage(props) {
   const pages = {
-    postMediaContent: 'Post Media Content'
+    postMediaContent: 'Post Media Content',
+    viewMediaContent: id => `View Media Content Post ${id}`,
+    editMediaContent: id => `Edit Media Content Post ${id}`,
+    deleteMediaContent: id => `Delete Media Content Post ${id}`,
   };
   const posts = [...Array(10).keys()].map(n =>
     ({ id: n + 1, title: `Post ${n + 1}`, description: `Post ${n + 1} description` }));
@@ -25,7 +29,7 @@ export default function MediaContentPage(props) {
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name='Media Content Feed' children={(localProps) =>
             <FlatList data={posts} renderItem={({ item }) =>
-              <TouchableOpacity onPress={() => console.log(`View ${item.title}`)}>
+              <TouchableOpacity onPress={() => localProps.navigation.push(pages.viewMediaContent(item.id))}>
                 <Card containerStyle={{
                   borderColor: props.theme.colors.border,
                   backgroundColor: props.theme.colors.card
@@ -46,6 +50,25 @@ export default function MediaContentPage(props) {
           {props.admin &&
             <Stack.Screen name={pages.postMediaContent} children={(localProps) =>
               <MediaContentForm {...props} {...localProps} />} />}
+          {posts.map(post => 
+            <Stack.Screen key={post.id} name={pages.viewMediaContent(post.id)} children={(localProps) =>
+              <AppPage {...props} {...localProps} nested>
+                {props.admin &&
+                  <Button {...props} {...localProps} text='Edit'
+                    onPress={() => localProps.navigation.push(pages.editMediaContent(post.id))} />}
+                {props.admin &&
+                  <Button {...props} {...localProps} text='Delete'
+                    onPress={() => localProps.navigation.push(pages.deleteMediaContent(post.id))} />}
+                <View style={SharedStyles.container}>
+                  <Text style={{ color: props.theme.colors.text }}>{localProps.route.name}</Text>
+                </View>
+              </AppPage>} />)}
+          {posts.map(post =>
+            <Stack.Screen key={post.id} name={pages.editMediaContent(post.id)} children={(localProps) =>
+              <MediaContentForm {...props} {...localProps} />} />)}
+          {posts.map(post =>
+            <Stack.Screen key={post.id} name={pages.deleteMediaContent(post.id)} children={(localProps) =>
+              <EmptyPage {...props} {...localProps} nested cancel />} />)}
         </Stack.Navigator>
       </NavigationContainer>
     </AppPage>
