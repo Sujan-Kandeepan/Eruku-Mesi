f3983451b09a7db9c317b6bf9da13d64791230c1
Sujan Kandeepan <kandeeps@mcmaster.ca>
2021-01-08 11:19:54 -0500

    Close keyboard on drawer open, indentation fix

--- components/AppPage.js
+++ components/AppPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Keyboard, Text, View } from 'react-native';
 import { Icon } from 'react-native-elements';
+import { useIsDrawerOpen } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -12,7 +13,11 @@ const Stack = createStackNavigator();
 
 // Common layout/logic for all app pages
 export default function AppPage({ cancel, children, navigation, nested, onReturn, route, tab, theme }) {
-    return (
+  // References: https://stackoverflow.com/a/64249361 and https://stackoverflow.com/a/39772206
+  const drawerOpen = navigation.toggleDrawer && useIsDrawerOpen();
+  React.useEffect(() => { if (drawerOpen) Keyboard.dismiss() }, [drawerOpen]);
+
+  return (
     // Workaround for displaying header within drawer nav (framework limitation):
     // https://github.com/react-navigation/react-navigation/issues/1632#issuecomment-305291994
     <NavigationContainer style={SharedStyles.container} theme={theme} independent>
@@ -28,13 +33,13 @@ export default function AppPage({ cancel, children, navigation, nested, onReturn
                   onPress={() => navigation.openDrawer()} />
               </Text>
             </View>
-          }} children={() => // Display back button if nested, then children
-            <>
-              {nested && !tab &&
-                <Button theme={theme} text={cancel ? 'Cancel' : 'Go Back'}
-                  onPress={(...args) => { navigation.pop(); if (onReturn) onReturn(...args); }} />}
-              {children}
-            </>} />
+        }} children={() => // Display back button if nested, then children
+          <>
+            {nested && !tab &&
+              <Button theme={theme} text={cancel ? 'Cancel' : 'Go Back'}
+                onPress={(...args) => { navigation.pop(); if (onReturn) onReturn(...args); }} />}
+            {children}
+          </>} />
       </Stack.Navigator>
     </NavigationContainer>
   );
