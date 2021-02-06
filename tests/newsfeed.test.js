fa8dfe96c60f071df2e2f63caf140e601dd4045a
Sujan Kandeepan <kandeeps@mcmaster.ca>
2021-02-06 01:01:40 -0500

    Remaining testcases for news story form

--- tests/newsfeed.test.js
+++ tests/newsfeed.test.js
@@ -1,10 +1,14 @@
-import { fetchNewsStories } from '../components/functions/NewsStoryFunctions';
+import { fetchNewsStories, submitNewsStory } from '../components/functions/NewsStoryFunctions';
 
 require('jest-fetch-mock').enableMocks();
 
 describe('News Feed', () => {
+  beforeEach(() => {
+    fetch.resetMocks()
+  });
+
   test('Fetches news stories', done => {
-    let props = { baseURL: 'http://localhost:4000', snackbar: () => { } };
+    let props = { snackbar: () => { } };
     let stories = [];
     let setStories = value => stories = value;
     fetch.mockResponseOnce(JSON.stringify(
@@ -23,6 +27,7 @@ describe('News Feed', () => {
     ));
     fetchNewsStories(props, setStories, () => {
       try {
+        expect(fetch.mock.calls.length).toEqual(1);
         expect(stories.length).toBe(1);
         expect(stories[0]).toMatchObject({
           title: 'My First News Story',
@@ -33,5 +38,49 @@ describe('News Feed', () => {
         done(error);
       }
     });
+  });
+
+  test('Validates news story form', () => {
+    let message = '';
+    let props = { snackbar: value => message = value };
+    let saving = false;
+    let setSaving = value => saving = value;
+    submitNewsStory(props, '', 'content', setSaving);
+    expect(message.toLowerCase()).toContain('title');
+    submitNewsStory(props, 'title', '', setSaving);
+    expect(message.toLowerCase()).toContain('content');
+    expect(saving).toBe(false);
+    expect(fetch.mock.calls.length).toEqual(0);
+  });
+
+  test('Makes request to create news story', () => {
+    let message = '';
+    let props = { snackbar: value => message = value };
+    let saving = false;
+    let setSaving = value => saving = value;
+    fetch.mockResponseOnce(JSON.stringify({}));
+    submitNewsStory(props, 'title', 'content', setSaving);
+    expect(saving).toBe(true);
+    expect(fetch.mock.calls.length).toEqual(1);
+    expect(fetch.mock.calls[0][0]).toContain('newsStories/add');
+    expect(fetch.mock.calls[0][1].method).toBe('POST');
+    expect(JSON.parse(fetch.mock.calls[0][1].body)).toMatchObject({ title: 'title', content: 'content' });
+  });
+
+  test('Makes request to update news story', () => {
+    let message = '';
+    let props = {
+      snackbar: value => message = value,
+      payload: { id: 1, title: 'title', content: ['content'] }
+    };
+    let saving = false;
+    let setSaving = value => saving = value;
+    fetch.mockResponseOnce(JSON.stringify({}));
+    submitNewsStory(props, 'title', 'content', setSaving);
+    expect(saving).toBe(true);
+    expect(fetch.mock.calls.length).toEqual(1);
+    expect(fetch.mock.calls[0][0]).toContain('newsStories/edit/1');
+    expect(fetch.mock.calls[0][1].method).toBe('POST');
+    expect(JSON.parse(fetch.mock.calls[0][1].body)).toMatchObject({ title: 'title', content: 'content' });
   });
 });
