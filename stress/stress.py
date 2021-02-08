d534fb2db8373129c4e79fb1180b0cd1ee4332ab
choij12 <choij12@mcmaster.ca>
2021-02-07 21:13:20 -0500

    replace rm command and format of the stress test output

--- stress/stress.py
+++ stress/stress.py
@@ -1,12 +1,22 @@
-import os
+import os, errno
 
 """
 Beginning of Test Cases
 Each stress test will be run and the output will
 be outputed to report.json
 """
-os.system('rm report.json')
-os.system('echo "---- Beginning of Tests ----\n" >> report.json')
+
+filename = 'report.txt'
+
+# deletes report.txt only if it already exists
+try:
+    os.remove(filename)
+except OSError as e: 
+	if e.errno != errno.ENOENT: # checks ENOENT = errno.ENOENT = no such file exists 
+		raise # only executed when a different error occurred 
+
+os.system('echo "---- Beginning of Tests ----\n"')
+
 
 requests = ["DELETE", "GET", "GET_2", "POST", "POST_2"]
 routes = [
@@ -23,13 +33,12 @@ routes = [
 		]
 ext = ".yaml"
 
-
 count = 0
 total = len(routes) * len(requests)
 for route in routes:
 	for request in requests:
 		count += 1
 		file = route + "/" + request + ext
-		os.system('echo "---- Sress Tesing" ' + file + " " + str(count) + "/" + str(total) + " ----")
-		os.system('echo "---- Sress Tesing" ' + file + " " + str(count) + "/" + str(total) + " ---- >> report.json" )
-		os.system('artillery run --config config.yaml ' + file + " >> report.json")
+		print("---- Stress Testing " + file + " " + str(count) + "/" + str(total) + " ----")
+		os.system('echo "---- Stress Tesing" ' + file + " " + str(count) + "/" + str(total) + " ---- >> report.txt" )
+		os.system('artillery run --config config.yaml ' + file + " >> report.txt")
