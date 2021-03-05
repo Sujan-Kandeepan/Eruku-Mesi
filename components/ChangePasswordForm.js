9c23634096fcb6203c6e671fe3f81871c155e0c3
Sujan Kandeepan <kandeeps@mcmaster.ca>
2021-03-04 19:50:34 -0500

    Refactored nested settings forms into separate files

---
+++ components/ChangePasswordForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import AppPage from './AppPage';
+import { Button, Header, SimpleInput } from '../shared/SharedComponents';
+import { validPassword } from '../shared/SharedFunctions';
+
+// Form for changing password in separate component to reset state on exit
+export default function ChangePasswordForm(props) {
+  // State variables for form fields (two-way data binding)
+  const [oldPassword, setOldPassword] = React.useState('');
+  const [newPassword, setNewPassword] = React.useState('');
+  const [confirmPassword, setConfirmPassword] = React.useState('');
+  const [passwordError, setPasswordError] = React.useState('');
+  // Form validation checks for password change
+  const fakePasswordChange = (navigation) => {
+    if (oldPassword.trim() == '') {
+      setPasswordError('Please enter your old password.');
+    } else if (newPassword.trim() == '') {
+      setPasswordError('Please specify a new password.');
+    } else if (confirmPassword.trim() == '') {
+      setPasswordError('Please re-enter your new password.');
+    } else if (oldPassword !== 'Test123!') {
+      setPasswordError('Old password entered is incorrect.');
+      console.log(oldPassword);
+    } else if (newPassword !== confirmPassword) {
+      setPasswordError('Entered passwords do not match.');
+    } else if (validPassword(newPassword, setPasswordError)) {
+      setPasswordError('');
+      navigation.pop();
+    }
+  };
+  return (
+    <AppPage {...props} nested cancel scroll>
+      <Header {...props} label text={'Old Password'} />
+      <SimpleInput {...props} left autoCompleteType='password' password
+        value={oldPassword} onChangeText={value => setOldPassword(value)} />
+      <Header {...props} label text={'New Password'} />
+      <SimpleInput {...props} left autoCompleteType='password' password
+        value={newPassword} onChangeText={value => setNewPassword(value)} />
+      <Header {...props} label text={'Confirm Password'} />
+      <SimpleInput {...props} left autoCompleteType='password' password
+        value={confirmPassword} onChangeText={value => setConfirmPassword(value)} />
+      <Text style={{ color: props.theme.colors.dangerText, paddingTop: 10, textAlign: 'center' }}>
+        {passwordError}
+      </Text>
+      <Button {...props} color='accent' text='Submit'
+        onPress={() => fakePasswordChange(props.navigation)} />
+    </AppPage>
+  );
+};
---
+++ components/PersonalInformationForm.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import AppPage from './AppPage';
+import { Button, Header, SimpleInput } from '../shared/SharedComponents';
+import { validEmail, validPhone } from '../shared/SharedFunctions';
+
+// Form for editing user info in separate component to reset state on exit
+export default function PersonalInformationForm(props) {
+  // State variables for form fields (two-way data binding)
+  const [username, setUsername] = React.useState(props.user.username);
+  const [firstName, setFirstName] = React.useState(props.user.firstName);
+  const [lastName, setLastName] = React.useState(props.user.lastName);
+  const [phone, setPhone] = React.useState(props.user.phone);
+  const [email, setEmail] = React.useState(props.user.email);
+  const [editInfoError, setEditInfoError] = React.useState('');
+  // Form validation and fake availability checks for editing personal info
+  const fakeEditInfo = (navigation) => {
+    if (username.trim() == '') {
+      setEditInfoError('Please specify a username.');
+    } else if (firstName.trim() == '' || lastName.trim() == '') {
+      setEditInfoError('Please enter your first and last name.');
+    } else if (phone.trim() == '') {
+      setEditInfoError('Please enter your phone number.');
+    } else if (email.trim() == '') {
+      setEditInfoError('Please enter your email address.');
+    } else if (!validPhone(phone)) {
+      setEditInfoError('Please enter a valid phone number.');
+    } else if (!validEmail(email)) {
+      setEditInfoError('Please enter a valid email address.');
+    } else {
+      setEditInfoError('');
+      props.setUser({ ...props.user, username, firstName, lastName, phone, email });
+      navigation.pop();
+    }
+  };
+  return (
+    <AppPage {...props} nested cancel scroll>
+      <Header {...props} label text={'Username'} />
+      <SimpleInput {...props} left autoCompleteType='username' autoCapitalize='none'
+        value={username} onChangeText={value => setUsername(value)} />
+      <Header {...props} label text={'First Name'} />
+      <SimpleInput {...props} left autoCompleteType='name'
+        value={firstName} onChangeText={value => setFirstName(value)} />
+      <Header {...props} label text={'Last Name'} />
+      <SimpleInput {...props} left autoCompleteType='name'
+        value={lastName} onChangeText={value => setLastName(value)} />
+      <Header {...props} label text={'Phone Number'} />
+      <SimpleInput {...props} left autoCompleteType='tel' keyboardType='number-pad'
+        value={phone} onChangeText={value => setPhone(value)} />
+      <Header {...props} label text={'Email Address'} />
+      <SimpleInput {...props} left autoCompleteType='email' autoCapitalize='none'
+        value={email} onChangeText={value => setEmail(value)} />
+      <Text style={{ color: props.theme.colors.dangerText, paddingTop: 10, textAlign: 'center' }}>
+        {editInfoError}
+      </Text>
+      <Button {...props} color='accent' text='Submit'
+        onPress={() => fakeEditInfo(props.navigation)} />
+    </AppPage>
+  );
+};
